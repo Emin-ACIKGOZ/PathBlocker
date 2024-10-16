a98ed569370e57e5d70832cd3736d00fbd211f59
Reshman0 <bekir.berk844@gmail.com>
2024-10-16 13:08:33 +0300

    Update PathBlockerState.java

--- src/PathBlockerState.java
+++ src/PathBlockerState.java
@@ -36,12 +36,12 @@ public class PathBlockerState extends State {
         try {
             Scanner sc = new Scanner(file);
 
-            List<List<Integer>> outer = new ArrayList(); // i used 2d list because of the unknown dimensions of the .txt file
+            List<List<Integer>> outer = new ArrayList<>(); // i used 2d list because of the unknown dimensions of the .txt file
 
             while (sc.hasNextLine()){
                 String line = sc.nextLine();
 
-                List<Integer> inner = new ArrayList();
+                List<Integer> inner = new ArrayList<>();
 
                 for (int i = 0;i<line.length();i++){
                     Integer temp = Character.getNumericValue(line.charAt(i));
@@ -113,12 +113,12 @@ public class PathBlockerState extends State {
     }
 
     private int getMovementDistance(int dx, int dy) {
-        //Amount of distance travelled in one direction
+        // Amount of distance travelled in one direction
         int distance = 0;
         int x = playerX;
         int y = playerY;
 
-        //Move until the goal or an obstacle is encountered
+        // Move until the goal or an obstacle is encountered
         while (x + dx >= 0 && x + dx < matrix[0].length && y + dy >= 0 && y + dy < matrix.length
                 && matrix[y + dy][x + dx] == 0) {
             x += dx;
@@ -126,11 +126,6 @@ public class PathBlockerState extends State {
             distance++;
         }
         return distance;
-
-        // note from ugur: since i have changed the
-        // logic of boolean matrix to int, i assumed that the character
-        // should keep moving until the road is not empty (0). But this logic might be wrong i am not so sure.
-        // so we might need to adjust the last condition on the loop.
     }
 
     @Override
@@ -142,51 +137,14 @@ public class PathBlockerState extends State {
         return this;
     }
 
-    private PathBlockerState movePlayer(Direction direction) {
-        //NEEDS TO BE IMPLEMENTED PROPERLY
-        // boolean[][] newMatrix = cloneMatrix();
-        // Mark current position as visited (true)
-        // newMatrix[playerY][playerX] = true;
+    private PathBlockerState movePlayer(int newX, int newY) {
+        int[][] newMatrix = cloneMatrix();
+        // Mark current position as visited (1)
+        newMatrix[playerY][playerX] = 1;
         // Set new position
-        // return new PathBlockerState(newMatrix, newX, newY, goalX, goalY, );
-        //If the current location of the player matches the goal's location, stop moving and set goalReached to true
-//        if(x == goalX && y == goalY){
-//            goalReached = true;
-//            return distance;
-//        }
-
-        int[][] newMatrix = this.cloneMatrix();
-        int newPlayerX = playerX;
-        int newPlayerY = playerY;
-
-        switch(direction){
-
-            case Direction.RIGHT:
-
-                while (newPlayerX < matrix[0].length-1 && matrix[newPlayerY][newPlayerX+1] != 1){ // stop if you are at the bound or your next cell is a wall
-
-                    if (newPlayerX == goalX && newPlayerY == goalY){ // you reached the goal
-
-                        boolean newGoalReached = true;
-
-                        return new PathBlockerState(newMatrix, newPlayerX, newPlayerY, goalX, goalY, newGoalReached);
-
-                    }
-
-                    matrix[newPlayerY][newPlayerX+1] = 2;
-                    matrix[newPlayerY][newPlayerX] = 1;
-
-                }
-
-
-
-
-
-
-        }
-
-
-        return null;
+        boolean newGoalReached = newX == goalX && newY == goalY;
+        newMatrix[newY][newX] = 2;
+        return new PathBlockerState(newMatrix, newX, newY, goalX, goalY, newGoalReached);
     }
 
     private int[][] cloneMatrix() {
@@ -200,7 +158,8 @@ public class PathBlockerState extends State {
     @Override
     public State undoAction(Action action) {
         if (action instanceof PathAction) {
-            //needs to be implemented!
+            PathAction pathAction = (PathAction) action;
+            return movePlayer(playerX - pathAction.getDeltaX(), playerY - pathAction.getDeltaY());
         }
         return this;
     }
